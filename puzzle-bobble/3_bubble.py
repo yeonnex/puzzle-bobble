485a1d93d86fd7b0283d12d447510a78ebe8307a
yeonnex <yeonnex@gmail.com>
2021-09-26 21:50:28 +0900

    버블이미지(png 포맷)와 버블이미지 담은 리스트 생성

--- puzzle-bobble/3_bubble.py
+++ puzzle-bobble/3_bubble.py
@@ -1,3 +1,5 @@
+# 버블 이미지 설정 / map 설정
+
 import pygame
 import os
 pygame.init()
@@ -10,7 +12,16 @@ clock = pygame.time.Clock()  # 게임 프레임 설정을 위한 객체 생성
 # 배경 이미지 불러오기
 current_path = os.path.dirname(__file__) # 현재 실행하고 있는 파일의 디렉토리이름을 받아옴
 background = pygame.image.load(os.path.join(current_path, 'background.png'))
-# print(background)
+
+# 버블 이미지 불러오기
+bubble_images = [
+    pygame.image.load(os.path.join(current_path, 'red.png')).convert_alpha(),
+    pygame.image.load(os.path.join(current_path, 'yellow.png')).convert_alpha(),
+    pygame.image.load(os.path.join(current_path, 'blue.png')).convert_alpha(),
+    pygame.image.load(os.path.join(current_path, 'green.png')).convert_alpha(),
+    pygame.image.load(os.path.join(current_path, 'purple.png')).convert_alpha(),
+    pygame.image.load(os.path.join(current_path, 'black.png')).convert_alpha()
+]
 
 running = True # 게임 루프를 위한 변수, running. running변수가 True인 동안 계속해서 게임루프가 돌아감
 while running:
@@ -24,4 +35,3 @@ while running:
     pygame.display.update() # 업데이트 꼭 해줘야 함. blit() 만 하고 업데이트 하지 않으면 배경화면이 눈에 보이지 않음!
 
 pygame.quit() # 반복문 탈출, pygame 종료. 프로그램 종료
-
